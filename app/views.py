42e6accec103f73b6184a25ec5ba4bb892812946
KariukiAbel <abelmuriithi@gmail.com>
2022-06-09 11:51:33 +0300

    added a view in views.py file and created the urls.py file for our routing in app

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import AppModel
+from serializer import AppModelSerializer
 
 # Create your views here.
+class List(generics.ListAPIView):
+    queryset = AppModel.objects.all()
+    serializer_class = AppModelSerializer
